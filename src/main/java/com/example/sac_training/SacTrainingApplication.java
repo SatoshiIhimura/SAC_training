619389a52da9ffd3db4cc54ce6c19f771a0237ad
WIN-937N7CTGEB3\traininguser <satoshi.ishimura@skywill.jp>
2026-08-20 11:35:08 +0900

    Initial commit

---
+++ src/main/java/com/example/sac_training/SacTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.sac_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sac_training/SacTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sac_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
